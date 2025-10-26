a26ff80b8ef58f1b4e0e97fc04ff2b672ed304fe
darraghd493 <60071719+darraghd493@users.noreply.github.com>
2025-10-26 03:07:48 +0000

    fix: anomalous GlobalProcessor cache/insert error

--- src/processor/GlobalProcessor.ts
+++ src/processor/GlobalProcessor.ts
@@ -8,8 +8,8 @@ import { BaseProcessor, Insert } from "./BaseProcessor";
 
 class GlobalProcessor extends BaseProcessor {
     private inserts: Insert[] = [];
-    private insertsCache: { [key: string]: number } = {};
-
+    private insertsCache: { [key: string]: string } = {};
+    
     public process(path: string): string {
         if (this.cache[path]) {
             console.log(this.generateSpace(), pc.yellow(`Cache hit for ${path} +!`));
@@ -17,7 +17,7 @@ class GlobalProcessor extends BaseProcessor {
         }
 
         if (this.stack.toArray().includes(path)) {
-            throw new Error("Circular dependency detected: " + this.toString());
+            throw new Error("Circular dependency detected: " + this.toString() + ` -> ${path}`);
         }
 
         this.stack.push(path);
@@ -28,14 +28,14 @@ class GlobalProcessor extends BaseProcessor {
 
         console.log(this.generateSpace(), pc.green(`Processed ${path}!`));
         this.stack.pop();
-        
+
         return this.cache[path];
     }
 
     public insert(path: string): string {
         if (this.insertsCache[path]) {
             console.log(this.generateSpace(), pc.yellow(`Cache hit for ${path} +!`));
-            return this.cache[path];
+            return this.insertsCache[path];
         }
         console.log(this.generateSpace(), pc.gray("Inserting script reference..."));
         const parser = parse(this.process(path));
@@ -44,11 +44,11 @@ class GlobalProcessor extends BaseProcessor {
             name,
             body: parser.body
         });
-        this.insertsCache[path] = this.inserts.length - 1;
+        this.insertsCache[path] = name;
         console.log(this.generateSpace(), pc.gray("Inserted script reference!"));
         return name;
     }
-    
+
     protected handleInserts(parser: Chunk): void {
         if (this.stack.length > 1) return;
         while (this.inserts.length > 0) {
